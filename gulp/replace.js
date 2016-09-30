dafd201ddee409f235336f08546739de3590650a
耿霄 <gengxiao@meituan.com>
2016-09-30 19:53:31 +0800

    replace

---
+++ gulp/replace.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (gulp, gulpConfig, plugins) {
+
+    // gulp.task('templates', function () {
+    //     var common = gulpConfig.common;
+
+    //     return gulp.src([common.buildPagesFile])
+    //         .pipe(replace(/foo(.{3})/g, '$1foo'))
+    //         .pipe(gulp.dest('build/file.txt'));
+    // });
+};
