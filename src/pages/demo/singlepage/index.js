c22bfd3916a2d064188bc3a4c433993d7328c463
耿霄 <gengxiao@meituan.com>
2016-10-21 16:58:08 +0800

    fix

--- src/pages/demo/singlepage/index.js
+++ src/pages/demo/singlepage/index.js
@@ -9,7 +9,7 @@
         bizself: '/api/v1/mta/sc/bizself',
         upload: '/api/v1/mta/sc/bizself/certificate/upload'
     };
-    var vue = new Vue({
+    var vm = new Vue({
         el: '#container',
         data: {
             // 配置数据
@@ -50,12 +50,12 @@
 
             // 获取省份数据
             getProvince: function () {
-                var vm = this;
+                var _vm = this;
                 var provinceUrl = apiHost + urlList.province;
                 Vue.http.get(provinceUrl, null, null).then(function (response) {
                     var result = response.json();
                     if (result.status === 0) {
-                        vm.config.provinceList = result.data;
+                        _vm.config.provinceList = result.data;
                     } else {
                         alert('接口请求失败');
                     }
@@ -66,19 +66,19 @@
 
             // 获取子区域数据
             getSubarea: function (id, type) {
-                var vm = this;
+                var _vm = this;
                 var subareaUrl = apiHost + urlList.subarea + '/' + id;
                 Vue.http.get(subareaUrl, null, null).then(function (response) {
                     var result = response.json();
                     if (result.status === 0) {
                         if (type === 'city') {
-                            vm.config.cityList = result.data;
-                            vm.config.locationList = [];
-                            vm.basicInfo.cityId = -1;
-                            vm.basicInfo.locationId = -1;
+                            _vm.config.cityList = result.data;
+                            _vm.config.locationList = [];
+                            _vm.basicInfo.cityId = -1;
+                            _vm.basicInfo.locationId = -1;
                         } else if (type === 'location') {
-                            vm.config.locationList = result.data;
-                            vm.basicInfo.locationId = -1;
+                            _vm.config.locationList = result.data;
+                            _vm.basicInfo.locationId = -1;
                         }
                     } else {
                         alert('接口请求失败');
@@ -90,11 +90,11 @@
 
             // 地区改动
             changeRegion: function (type) {
-                var vm = this;
+                var _vm = this;
                 if (type === 'province') {
-                    vm.getSubarea(vm.basicInfo.provinceId, 'city');
+                    _vm.getSubarea(_vm.basicInfo.provinceId, 'city');
                 } else if (type === 'city') {
-                    vm.getSubarea(vm.basicInfo.cityId, 'location');
+                    _vm.getSubarea(_vm.basicInfo.cityId, 'location');
                 }
             },
 
@@ -129,7 +129,7 @@
                 };
 
                 var bizselfUrl = apiHost + urlList.bizself;
-                var vm = this;
+                var _vm = this;
                 Vue.http.post(bizselfUrl, param, null).then(function (response) {
                     var result = response.json();
                     if (result.status === 0) {
@@ -138,7 +138,7 @@
                         var clientHeight = window.screen.height;
                         document.getElementById('container').style.minHeight = clientHeight + 'px';
 
-                        vm.config.submitForm = true;
+                        _vm.config.submitForm = true;
                     } else {
                         alert('接口请求失败');
                     }
@@ -159,7 +159,7 @@
                     processData: false,
                     contentType: false
                 };
-                var vm = this;
+                var _vm = this;
                 formData.append('file', file);
 
                 // 只选择图片文件
@@ -167,7 +167,7 @@
                 Vue.http.post(fileUrl, formData, options).then(function (response) {
                     var result = response.json();
                     if (result.status === 0) {
-                        vm.certificateInfo.push(result.data);
+                        _vm.certificateInfo.push(result.data);
                     } else {
                         alert('上传失败');
                     }
@@ -185,5 +185,5 @@
             }
         }
     });
-    vue.init();
+    vm.init();
 }());
